59563789457a9ecaef55ab600a89df1dae18a014
GirardiNicolas <ngirardi@fi.uba.ar>
2022-11-10 22:37:36 -0300

    reemplazo en casos de uso 12

--- src/main/test/edu/fiuba/algo3/entrega_0/CasoDeUso12.java
+++ src/main/test/edu/fiuba/algo3/entrega_0/CasoDeUso12.java
@@ -10,16 +10,17 @@ public class CasoDeUso12 {
     @Test
     public void UnNexoMineralRecibeDemasiadoDañoySoloRegeneraEscudo(){
 
-        HitPoints HPmock = mock(HitPoints.class);
-        Edificio edificio = new NexoMineral(HPmock); //podría ser cualquier edificio
+        //HitPoints HPmock = mock(HitPoints.class);
+        HitPoints HP = new HPProtoss(250,250);
+        Edificio edificio = new NexoMineral(HP); //podría ser cualquier edificio
 
         edificio.recibirDaño(300);
-        when(HPmock.vida()).thenReturn(200);
-        when(HPmock.escudo()).thenReturn(250);
+        //when(HPmock.vida()).thenReturn(200);
+        //when(HPmock.escudo()).thenReturn(250);
         for(int i=0; i<10;i++) {
             edificio.ejecutarTurno();
         }
-        verify(HPmock,times(10)).regenerar();
+        //verify(HPmock,times(10)).regenerar();
 
         assertEquals(200,edificio.vidaActual());
         assertEquals(250,edificio.escudoActual());
@@ -28,16 +29,17 @@ public class CasoDeUso12 {
     @Test
     public void UnPilonRecibeDemasiadoDañoySoloRegeneraEscudo(){
 
-        HitPoints HPmock = mock(HitPoints.class);
-        Edificio edificio = new Pilon(0,HPmock); //podría ser cualquier edificio
+        //HitPoints HPmock = mock(HitPoints.class);
+        HitPoints HP = new HPProtoss(300,300);
+        Edificio edificio = new Pilon(0,HP); //podría ser cualquier edificio
 
         edificio.recibirDaño(400);
-        when(HPmock.vida()).thenReturn(200);
-        when(HPmock.escudo()).thenReturn(300);
+        //when(HPmock.vida()).thenReturn(200);
+        //when(HPmock.escudo()).thenReturn(300);
         for(int i=0; i<10;i++) {
             edificio.ejecutarTurno();
         }
-        verify(HPmock,times(10)).regenerar();
+        //verify(HPmock,times(10)).regenerar();
 
         assertEquals(200,edificio.vidaActual());
         assertEquals(300,edificio.escudoActual());
@@ -46,34 +48,36 @@ public class CasoDeUso12 {
     @Test
     public void UnAsimiladorRecibeDemasiadoDañoySoloRegeneraEscudo(){
 
-        HitPoints HPmock = mock(HitPoints.class);
-        Edificio edificio = new Asimilador(0,HPmock); //podría ser cualquier edificio
+        //HitPoints HPmock = mock(HitPoints.class);
+        HitPoints HP = new HPProtoss(450,450);
+        Edificio edificio = new Asimilador(0,HP); //podría ser cualquier edificio
 
         edificio.recibirDaño(650);
-        when(HPmock.vida()).thenReturn(200);
-        when(HPmock.escudo()).thenReturn(450);
+        //when(HPmock.vida()).thenReturn(250);
+        //when(HPmock.escudo()).thenReturn(450);
         for(int i=0; i<10;i++) {
             edificio.ejecutarTurno();
         }
-        verify(HPmock,times(10)).regenerar();
+        //verify(HPmock,times(10)).regenerar();
 
-        assertEquals(200,edificio.vidaActual());
+        assertEquals(250,edificio.vidaActual());
         assertEquals(450,edificio.escudoActual());
 
     }
     @Test
     public void UnAccesoRecibeDemasiadoDañoySoloRegeneraEscudo(){
 
-        HitPoints HPmock = mock(HitPoints.class);
-        Edificio edificio = new Acceso(0,HPmock); //podría ser cualquier edificio
+        //HitPoints HPmock = mock(HitPoints.class);
+        HitPoints HP = new HPProtoss(500,500);
+        Edificio edificio = new Acceso(0,HP); //podría ser cualquier edificio
 
         edificio.recibirDaño(800);
-        when(HPmock.vida()).thenReturn(200);
-        when(HPmock.escudo()).thenReturn(500);
+        //when(HPmock.vida()).thenReturn(200);
+        //when(HPmock.escudo()).thenReturn(500);
         for(int i=0; i<10;i++) {
             edificio.ejecutarTurno();
         }
-        verify(HPmock,times(10)).regenerar();
+        //verify(HPmock,times(10)).regenerar();
 
         assertEquals(200,edificio.vidaActual());
         assertEquals(500,edificio.escudoActual());
@@ -82,16 +86,17 @@ public class CasoDeUso12 {
     @Test
     public void UnPuertoEstelarRecibeDemasiadoDañoySoloRegeneraEscudo(){
 
-        HitPoints HPmock = mock(HitPoints.class);
-        Edificio edificio = new PuertoEstelar(0,HPmock); //podría ser cualquier edificio
+        //HitPoints HPmock = mock(HitPoints.class);
+        HitPoints HP = new HPProtoss(600,600);
+        Edificio edificio = new PuertoEstelar(0,HP); //podría ser cualquier edificio
 
         edificio.recibirDaño(1000);
-        when(HPmock.vida()).thenReturn(200);
-        when(HPmock.escudo()).thenReturn(600);
+        //when(HPmock.vida()).thenReturn(200);
+        //when(HPmock.escudo()).thenReturn(600);
         for(int i=0; i<10;i++) {
             edificio.ejecutarTurno();
         }
-        verify(HPmock,times(10)).regenerar();
+        //verify(HPmock,times(10)).regenerar();
 
         assertEquals(200,edificio.vidaActual());
         assertEquals(600,edificio.escudoActual());
