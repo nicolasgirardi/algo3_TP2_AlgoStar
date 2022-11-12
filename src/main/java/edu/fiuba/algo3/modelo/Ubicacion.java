df663d490abeae2b6f8af05cac697fc991be7776
AbrahamOsco <aosco@fi.uba.ar>
2022-11-12 17:35:58 -0300

    Se avanza con el casoDe uso 3

---
+++ src/main/java/edu/fiuba/algo3/modelo/Ubicacion.java
@@ -0,0 +1,19 @@
+package edu.fiuba.algo3.modelo;
+
+public class Ubicacion {
+
+    Interactuable interactuable;
+    EstadoSuelo estado;
+
+    public Ubicacion(){
+
+    }
+    public void asignarRecurso(Volcan volcan) {
+
+    }
+
+    public void asignarEdifico(Asimilador asimilador){
+
+    }
+
+}
--- src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso3.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso3.java
@@ -1,4 +1,26 @@
 package edu.fiuba.algo3.entrega_1;
 
+import edu.fiuba.algo3.modelo.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class CasoDeUso3 {
+
+    @Test
+    public void testAsimiladorSoloSePuedeConstruirSobreElGas(){
+        Ubicacion ubicacion = new Ubicacion();
+        Volcan volcan = new Volcan();
+
+        ubicacion.asignarRecurso(volcan);
+
+        Asimilador unAsimilador = new Asimilador();
+
+        assertDoesNotThrow( ()-> {
+            ubicacion.asignarEdifico(unAsimilador);
+        });
+    }
+
+
 }
