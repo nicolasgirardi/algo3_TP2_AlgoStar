4292e827fb245a899c6bdf11ba312c188bd3bdaf
GirardiNicolas <ngirardi@fi.uba.ar>
2022-11-12 18:30:36 -0300

    Escribiendo tests caso de uso 18

--- src/main/test/edu/fiuba/algo3/entrega2/CasoDeUso18.java
+++ src/main/test/edu/fiuba/algo3/entrega2/CasoDeUso18.java
@@ -31,10 +31,10 @@ public class CasoDeUso18 {
         assertEquals(dañoEsperado,unidad.AtacarAire());
     }
     @Test
-    public void UnZerlingAtacaAireyCausa4Daño(){
+    public void UnZerlingAtacaAireyCausa0Daño(){
         Unidad unidad = new Zerling();
 
-        int dañoEsperado = 4;
+        int dañoEsperado = 0;
 
         assertEquals(dañoEsperado,unidad.AtacarAire());
     }
@@ -52,6 +52,39 @@ public class CasoDeUso18 {
         Unidad unidad = new Hidralisco();
 
         int dañoEsperado = 10;
+
+        assertEquals(dañoEsperado,unidad.AtacarAire());
+    }
+
+    @Test
+    public void UnMutaliscoAtacaTierrayCausa9Daño(){
+        Unidad unidad = new Mutalisco();
+
+        int dañoEsperado = 9;
+
+        assertEquals(dañoEsperado,unidad.AtacarAire());
+    }
+    @Test
+    public void UnMutaliscoAtacaAireyCausa9Daño(){
+        Unidad unidad = new Mutalisco();
+
+        int dañoEsperado = 9;
+
+        assertEquals(dañoEsperado,unidad.AtacarAire());
+    }
+    @Test
+    public void UnGuardianAtacaTierrayCausa25Daño(){
+        Unidad unidad = new Mutalisco();
+
+        int dañoEsperado = 25;
+
+        assertEquals(dañoEsperado,unidad.AtacarAire());
+    }
+    @Test
+    public void UnMutaliscoAtacaAireyCausa0Daño(){
+        Unidad unidad = new Mutalisco();
+
+        int dañoEsperado = 0;
 
         assertEquals(dañoEsperado,unidad.AtacarAire());
     }
