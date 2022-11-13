ead810c31843406b4989da8fcc7d2079637043e5
AbrahamOsco <aosco@fi.uba.ar>
2022-11-13 00:13:50 -0300

    Se testea, en el caso de uso15 que no se extrae mas recurso que el disponible

--- src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso15.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso15.java
@@ -25,13 +25,26 @@ public class CasoDeUso15 {
         int maximaCantidadGasExtraible = 5000;
         int cantidadDeTurnosParaSacarTodoElGas = 167;
         int cantidadGas = 0;
+        int cantidadGasExceso = 0;
+
         //Act
         for(int i = 0; i <=cantidadDeTurnosParaSacarTodoElGas ; i++ ){
             cantidadGas += extractor.extraer();
         }
-       //Assert
+
+        // Intentamos extraer 5 veces mas el Volcan
+
+        for(int i = 0; i < 5 ; i++){
+            cantidadGasExceso += extractor.extraer();
+        }
+
+
+        //Assert
         assertEquals(maximaCantidadGasExtraible, cantidadGas);
+
+        assertEquals(0,cantidadGasExceso );
     }
+
 
     @Test
     public void RazaZergDejaDeConseguirMineralCuandoDelNodoMineralCuandoSeQuedaSinUnidadesDeMineral(){
@@ -41,12 +54,25 @@ public class CasoDeUso15 {
         int maximaCantidadGasExtraible = 2000;
         int cantidadDeTurnosParaSacarTodoElGas = 200;
         int cantidadMineral = 0;
+        int cantidadMineralExceso = 0;
+
         //Act
+
         for(int i = 0; i <=cantidadDeTurnosParaSacarTodoElGas ; i++ ){
             cantidadMineral += zangano.extraer(nodoMineral);
         }
+
+        // Intentamos extraer 5 veces mas en el nodoMineral
+
+        for(int i = 0; i < 5 ; i++){
+            cantidadMineralExceso += zangano.extraer(nodoMineral);
+        }
+
+
         //Assert
         assertEquals( maximaCantidadGasExtraible, cantidadMineral);
+        assertEquals(0,cantidadMineralExceso );
+
     }
 
 
@@ -64,14 +90,23 @@ public class CasoDeUso15 {
         int maximaCantidadGasExtraible = 5000;
         int cantidadDeTurnosParaSacarTodoElGas = 250;
         int cantidadGas = 0;
-
+        int cantidadGasExceso = 0;
         //Act
         for(int i = 0; i <=cantidadDeTurnosParaSacarTodoElGas; i++ ){
             cantidadGas += asimilador.extraer();
         }
 
+        // Intentamos extraer 5 veces mas en el volcan
+
+        for(int i = 0; i < 5 ; i++){
+            cantidadGasExceso += asimilador.extraer();
+        }
+
         //Assert
         assertEquals(maximaCantidadGasExtraible, cantidadGas);
+
+        assertEquals(0, cantidadGasExceso);
+
     }
 
     @Test
@@ -87,14 +122,23 @@ public class CasoDeUso15 {
         int maximaCantidadGasExtraible = 2000;
         int cantidadDeTurnosParaSacarTodoElGas = 250;
         int cantidadGas = 0;
+        int cantidadGasExceso = 0;
 
         //Act
         for(int i = 0; i < cantidadDeTurnosParaSacarTodoElGas; i++ ){
             cantidadGas += nexoMineral.extraer();
         }
 
+        // Intentamos extraer 5 veces mas en el nexoMineral
+        for(int i = 0; i < 5 ; i++){
+            cantidadGasExceso += nexoMineral.extraer();
+        }
+
         //Assert
         assertEquals(maximaCantidadGasExtraible, cantidadGas);
+
+        assertEquals(0, cantidadGasExceso );
+
     }
 
 }
