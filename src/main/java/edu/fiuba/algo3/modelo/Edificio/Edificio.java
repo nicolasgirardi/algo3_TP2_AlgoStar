d361dd92aced723f61ae1a6ba05ad80cbc126a02
VickyA5 <vavalos@fi.uba.ar>
2022-11-15 12:15:35 -0300

    soluciono criadero

--- src/main/java/edu/fiuba/algo3/modelo/Edificio/Edificio.java
+++ src/main/java/edu/fiuba/algo3/modelo/Edificio/Edificio.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 public abstract class Edificio {
 
     private Ubicacion ubicacion;
-    private int turnosRestantesParaSerOperativo;
+    protected int turnosRestantesParaSerOperativo;
     private HitPoints hp;
 
     public Edificio(int turnosRestantesParaSerOperativo){
---
+++ src/main/test/edu/fiuba/algo3/entrega_2/CasoDeUso22.java
@@ -0,0 +1,32 @@
+package edu.fiuba.algo3.entrega_2;
+
+import edu.fiuba.algo3.modelo.Edificio.Criadero;
+import edu.fiuba.algo3.modelo.HitPoints.HPZerg;
+import edu.fiuba.algo3.modelo.HitPoints.HitPoints;
+import edu.fiuba.algo3.modelo.Unidad.Unidad;
+import edu.fiuba.algo3.modelo.Unidad.Zangano;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CasoDeUso22 {
+/*
+    @Test
+    public void testSeQuiereConstruirUnZanganoDeberiaTardar1Turno(){
+        Criadero criadero = new Criadero();
+        HitPoints HpZangano = new HPZerg(20);
+        Unidad zanganoUno = new Zangano(HpZangano);
+
+        assertEquals(,);
+    }
+
+
+    @Test
+    public void testUnZerlingAtacaAUnScoutQueEsUnaUnidadAireDeberiaTirarExcepcion(){
+
+
+       assertThrows(NoPuedeAtacarUnidadDeAireError.class, ()-> {
+            zerling.atacar(scout);
+        });
+    }*/
+}
