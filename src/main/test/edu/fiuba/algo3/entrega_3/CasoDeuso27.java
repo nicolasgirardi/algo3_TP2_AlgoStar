163f3f19061fd6b8f9f0d34076ef6ba807f006f9
wain02 <mwainwright@fi.uba.ar>
2022-11-19 17:36:11 -0300

    avanzo caso de uso 27

--- src/main/test/edu/fiuba/algo3/entrega_3/CasoDeuso27.java
+++ src/main/test/edu/fiuba/algo3/entrega_3/CasoDeuso27.java
@@ -1,14 +1,16 @@
 package edu.fiuba.algo3.entrega_3;
 
+import edu.fiuba.algo3.modelo.Atacable;
+import edu.fiuba.algo3.modelo.HitPoints.HPProtoss;
 import edu.fiuba.algo3.modelo.HitPoints.HPZerg;
+import edu.fiuba.algo3.modelo.HitPoints.HitPoints;
+import edu.fiuba.algo3.modelo.NoPuedeAplicarDanioUnidadTipoAire;
 import edu.fiuba.algo3.modelo.Raza.RazaZerg;
 import edu.fiuba.algo3.modelo.Recurso.RecursosInsuficientesError;
-import edu.fiuba.algo3.modelo.Unidad.Devorador;
-import edu.fiuba.algo3.modelo.Unidad.Mutalisco;
+import edu.fiuba.algo3.modelo.Unidad.*;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CasoDeuso27 {
     @Test
@@ -30,6 +32,47 @@ public class CasoDeuso27 {
         assertThrows( RecursosInsuficientesError.class, ()-> {
             mutalisco.evolucionarDevorador(raza) ; //busca un mutalisco y lo hace evolucionar .
         });
+    }
+    @Test
+    public void testUnDevoradorAtacaAUnScoutQueEsUnaUnidadDeSuperficieTierrayLeDeberiaCausar15Danio(){
+        //Arrange
+        HitPoints hpScout = new HPProtoss(60,100);
+        HitPoints hpEsperado = new HPProtoss(60,75);
+
+        Unidad devorador = new Devorador();
+        Atacable scout = new Scout(hpScout);
+
+
+        for (int i = 0; i < 4; i++){
+            devorador.ejecutarTurno();
+        }
+
+        //Act
+        devorador.atacar(scout);
+
+        //Assert
+        assertEquals(hpEsperado,hpScout);
+    }
+
+    @Test
+    public void testUnDevoradorAtacaAUnZealotQueEsUnaUnidadDeSuperficieTierraYNoLeDeberiaCausarDanio(){
+        //Arrange
+        HitPoints hpZealot = new HPProtoss(60,100);
+        HitPoints hpEsperado = new HPProtoss(60,100);
+
+        Unidad devorador = new Devorador();
+        Atacable zealot = new Zealot(hpZealot);
+
+
+        for (int i = 0; i < 4; i++){
+            devorador.ejecutarTurno();
+        }
+
+        //Act
+        devorador.atacar(zealot);//no le aplica danio Zealot es unidad terrestre
+
+        //Assert
+        assertEquals(hpEsperado,hpZealot);
     }
 
 }
