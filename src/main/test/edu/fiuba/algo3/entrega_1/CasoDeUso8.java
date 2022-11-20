f45e17ba1f18e4819948a9e51f3f747fab65c81f
VickyA5 <vavalos@fi.uba.ar>
2022-11-20 02:00:52 -0300

    corrijo el caso de uso 8

--- src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso8.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso8.java
@@ -144,7 +144,7 @@ public class CasoDeUso8 {
     }
 
     @Test
-    public void testRazaZergSeCreaUnCriaderoYUnaReservaDeReproduccionDespuesNoSeDeberiaPoderCrearUnCriaderoMas(){
+    public void testRazaZergSeCreaUnCriaderoDespuesNoSeDeberiaPoderCrearUnCriaderoMas(){
         //Arrange
         RazaZerg razaZerg = new RazaZerg();
         Criadero criadero = new Criadero();
@@ -152,8 +152,7 @@ public class CasoDeUso8 {
         Criadero criaderoExtra = new Criadero();
 
         //Act
-        razaZerg.agregarEdificio(criadero); // gasto 50 de mineral
-        razaZerg.agregarEdificio(reservaDeReproduccion); //gasta 150 de minerla
+        razaZerg.agregarEdificio(criadero); // gasto 200 de mineral
 
         //se quedo sin mineral lanza excepcion al intentar crear sin mineral un criadero.
 
