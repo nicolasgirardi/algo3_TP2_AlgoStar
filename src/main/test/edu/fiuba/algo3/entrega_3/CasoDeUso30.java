ad5925eb6a0830ca0ec0439e85fcb68d33cd17b7
GirardiNicolas <ngirardi@fi.uba.ar>
2022-11-21 15:19:50 -0300

    caso de uso 30 falta implementacion

--- src/main/test/edu/fiuba/algo3/entrega_3/CasoDeUso30.java
+++ src/main/test/edu/fiuba/algo3/entrega_3/CasoDeUso30.java
@@ -5,13 +5,30 @@ import edu.fiuba.algo3.modelo.HitPoints.HPProtoss;
 import edu.fiuba.algo3.modelo.HitPoints.HPZerg;
 import edu.fiuba.algo3.modelo.HitPoints.HitPoints;
 import edu.fiuba.algo3.modelo.NoPuedeAplicarDanioUnidadTipoAire;
-import edu.fiuba.algo3.modelo.Raza.RazaZerg;
+import edu.fiuba.algo3.modelo.Raza.*;
 import edu.fiuba.algo3.modelo.Recurso.RecursosInsuficientesError;
 import edu.fiuba.algo3.modelo.Unidad.*;
-import edu.fiuba.algo3.modelo.UnidadesRecurso.GestionRecurso;
+import edu.fiuba.algo3.modelo.UnidadesRecurso.*;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CasoDeUso30 {
 
+    @Test
+    public void SiSeTienenLaCapacidadMaximaOcupadaNoSePuedeCrearUnaNuevaUnidad(){
+        Raza razaZerg = new RazaZerg();
+        razaZerg.aumentarMineral(new GestionRecurso(7000));
+        for (int i =0 ;i<40;i++) {
+            razaZerg.agregarUnidad(new AmoSupremo());
+        }
+        for(int i =0;i<200;i++){
+            razaZerg.agregarUnidad(new Zangano());
+        }
+
+        assertThrows( CantidadInsuficienteError.class, ()-> {
+            razaZerg.agregarUnidad(new Zangano()) ;
+        });
+    }
 
 }
