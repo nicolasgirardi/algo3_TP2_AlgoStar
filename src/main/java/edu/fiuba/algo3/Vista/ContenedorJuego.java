534097aa6d0aacdf7b4148f2b53a0080c82e594d
Jonathan Huata Vasquez <jonathan_huata@outlook.com>
2022-11-30 22:39:05 -0300

    se agregar matriz del mapa  con imagenes

--- src/main/java/edu/fiuba/algo3/Vista/ContenedorJuego.java
+++ src/main/java/edu/fiuba/algo3/Vista/ContenedorJuego.java
@@ -1,5 +1,6 @@
 package edu.fiuba.algo3.Vista;
 
+import edu.fiuba.algo3.modelo.Edificio.Zerg.Criadero;
 import edu.fiuba.algo3.modelo.Juego.Juego;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -15,12 +16,14 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 
 public class ContenedorJuego extends BorderPane {
 
     public ContenedorJuego(Stage stage, Juego juego) {
-        Label labelHud = new Label("");
+        /*Label labelHud = new Label("");
         File fileHudJuego = new File("images/hudJuego.png");
         Image imagenHudJuego = new Image(fileHudJuego.toURI().toString(),1700, 1600, true, true );
 
@@ -32,18 +35,60 @@ public class ContenedorJuego extends BorderPane {
         canvas.getGraphicsContext2D().fillRect(0,0,1700,1600);
         Group unGrupo = new Group(canvas,labelHud);
         this.setBottom(unGrupo);
+        */
 
 
-
+        Criadero criadero = new Criadero();
 
         GridPane gridPane = new GridPane();
         gridPane.setBackground(new Background(new BackgroundFill(Color.AQUA, CornerRadii.EMPTY, Insets.EMPTY)));
         gridPane.setAlignment(Pos.CENTER);
-        for(int i = 0; i < 30; i++){
-            for(int j = 0; j < 20; j++){
-                gridPane.add(new Button(),i,j);
+        final int TAMANIO = 100;
+
+        // Tierra
+        File fileFondo = new File("images/tierra.png");
+        Image imagenRaza = new Image(fileFondo.toURI().toString(),TAMANIO, TAMANIO, true, true );
+
+        BackgroundImage primerBackGro = new BackgroundImage(
+                imagenRaza,
+                BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT
+        );
+
+
+
+
+
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                try {
+                    Button btn = new Button();
+                    //btn.setGraphic( new ImageView(imagenRaza) );
+                    btn.setMinSize(TAMANIO,TAMANIO);
+                    btn.setBackground( new Background(primerBackGro) );
+                    // btn.setStyle("-fx-background-color: #CA914B; -fx-border-width:1; -fx-border-color: #FFFFFF ");
+                    gridPane.add(btn, i, j);
+                } catch (Exception e){
+                    System.out.println(e);
+                }
             }
         }
+
+        // Criadero
+        File criaderoFile = new File("images/criadero.png");
+        Image criaderoImage = new Image(criaderoFile.toURI().toString(),TAMANIO, TAMANIO, true, true );
+        BackgroundImage criaderoBackGro = new BackgroundImage(
+                criaderoImage,
+                BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT
+        );
+        Button btnCriadero = new Button();
+        btnCriadero.setMinSize(TAMANIO,TAMANIO);
+        btnCriadero.setBackground( new Background(criaderoBackGro) );
+        gridPane.add(btnCriadero, 0, 0);
+
+
+
 
         this.setCenter(gridPane);
 
