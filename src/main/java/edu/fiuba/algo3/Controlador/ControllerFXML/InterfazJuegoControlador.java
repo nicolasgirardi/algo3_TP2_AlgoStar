3de1bd283c497d57f1f4886bd01e74f7e2f01f09
Jonathan Huata Vasquez <jonathan_huata@outlook.com>
2022-12-04 17:12:32 -0300

    se agrega interfaz grafica con sceneebuilder

---
+++ src/main/java/edu/fiuba/algo3/Controlador/ControllerFXML/InterfazJuegoControlador.java
@@ -0,0 +1,64 @@
+package edu.fiuba.algo3.Controlador.ControllerFXML;
+
+import javafx.event.ActionEvent;
+        import javafx.fxml.FXML;
+        import javafx.scene.control.Button;
+        import javafx.scene.input.MouseEvent;
+        import javafx.scene.layout.GridPane;
+        import javafx.scene.layout.VBox;
+
+public class InterfazJuegoControlador {
+
+    @FXML
+    private Button btnAccion1;
+
+    @FXML
+    private Button btnAccion10;
+
+    @FXML
+    private Button btnAccion11;
+
+    @FXML
+    private Button btnAccion12;
+
+    @FXML
+    private Button btnAccion2;
+
+    @FXML
+    private Button btnAccion3;
+
+    @FXML
+    private Button btnAccion4;
+
+    @FXML
+    private Button btnAccion5;
+
+    @FXML
+    private Button btnAccion6;
+
+    @FXML
+    private Button btnAccion7;
+
+    @FXML
+    private Button btnAccion8;
+
+    @FXML
+    private Button btnAccion9;
+
+    @FXML
+    private GridPane grPane;
+
+    @FXML
+    private VBox vBoxMenu;
+
+    @FXML
+    void btnTerminarTurno(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickTerminarTurno(MouseEvent event) {
+
+    }
+
+}
