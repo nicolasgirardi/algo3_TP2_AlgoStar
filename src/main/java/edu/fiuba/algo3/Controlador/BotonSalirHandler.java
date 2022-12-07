49fef07fce021b1b191889f798d9fc9a23ede784
GirardiNicolas <ngirardi@fi.uba.ar>
2022-12-06 21:26:22 -0300

    jugando con soniditos

--- src/main/java/edu/fiuba/algo3/Controlador/BotonSalirHandler.java
+++ src/main/java/edu/fiuba/algo3/Controlador/BotonSalirHandler.java
@@ -3,19 +3,24 @@ package edu.fiuba.algo3.Controlador;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.media.*;
 
 public class BotonSalirHandler implements EventHandler<ActionEvent> {
 
     private Button boton;
+    private AudioClip sonidito;
 
     public BotonSalirHandler(Button boton) {
         this.boton = boton;
         boton.setText("Salir");
+        //System.out.println(getClass().getResource("http://sndup.net/4psz").toString());
+        sonidito = new AudioClip("http://sndup.net/4psz");
     }
 
 
     @Override
     public void handle(ActionEvent actionEvent) {
-        System.exit(0);
+        sonidito.play();
+
     }
 }
