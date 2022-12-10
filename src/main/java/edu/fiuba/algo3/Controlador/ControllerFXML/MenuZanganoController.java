e524a93d55b947e36d4614cfa4935275b0e5db26
Jonathan Huata Vasquez <jonathan_huata@outlook.com>
2022-12-10 16:27:23 -0300

    se agrega menu zangano

---
+++ src/main/java/edu/fiuba/algo3/Controlador/ControllerFXML/MenuZanganoController.java
@@ -0,0 +1,53 @@
+package edu.fiuba.algo3.Controlador.ControllerFXML;
+
+import edu.fiuba.algo3.Vista.Botones.Unidades.BotonZangano;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+
+public class MenuZanganoController {
+
+    @FXML
+    public AnchorPane contenerdorMenu;
+
+    @FXML
+    public void onClickedMoverArriba(MouseEvent event) {
+
+    }
+
+    @FXML
+    public void onClickedMoverDerecha(MouseEvent event) {
+
+    }
+
+    @FXML
+    public void onClickedMoverIzquierda(MouseEvent event) {
+
+    }
+
+    @FXML
+    public void onClickedMutarACriadero(MouseEvent event) {
+
+    }
+
+    @FXML
+    public void onClickedMutarAEspiral(MouseEvent event) {
+
+    }
+
+    @FXML
+    public void onClickedMutarAGuarida(MouseEvent event) {
+
+    }
+
+    @FXML
+    public void onClickedMutarAReservaDeReproduccion(MouseEvent event) {
+
+    }
+
+    public void setElements(BotonZangano botonZangano, GridPane tablero ){
+
+    }
+
+}
